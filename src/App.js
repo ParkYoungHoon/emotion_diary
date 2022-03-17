0ab7685e33363bd60cc622ba1060c4dd352ad251
Tavi <secret9179@gmail.com>
2022-03-18 06:37:09 +0900

    Home  날짜관련 Component 추가

--- src/App.js
+++ src/App.js
@@ -38,9 +38,42 @@ const reducer = (state, action) => {
 export const DiaryStateContext = React.createContext();
 export const DiaryDispatchContext = React.createContext();
 
+const dummyData = [
+  {
+    id:1,
+    emotion:1,
+    content: "오늘의 일기 1번",
+    date : 20220318
+  },
+  {
+    id:2,
+    emotion:2,
+    content: "오늘의 일기 2번",
+    date : 20220319   
+  },
+  {
+    id:3,
+    emotion:3,
+    content: "오늘의 일기 3번",
+    date : 20220320 
+  },
+  {
+    id:4,
+    emotion:4,
+    content: "오늘의 일기 4번",
+    date : 20220321  
+  },
+  {
+    id:5,
+    emotion:5,
+    content: "오늘의 일기 5번",
+    date : 20220322   
+  }
+]
+
 function App() {
 
-  const [data, dispatch] = useReducer(reducer, []);
+  const [data, dispatch] = useReducer(reducer, dummyData);
 
   const dataId = useRef(0);
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,55 @@
+import { useState, useContext, useEffect } from "react";
+
+import MyHeadr from './../components/MyHeader';
+import MyButton from './../components/MyButton';
+import { DiaryStateContext } from "../App";
+
 const Home = () => {
+
+    const diaryList = useContext(DiaryStateContext);
+
+    const [data, setData] = useState([]);
+    const [curDate, setCurDate] = useState(new Date());
+    const headText = `${curDate.getFullYear()}년 ${curDate.getMonth() + 1}월`;
+
+    useEffect(()=> {
+        const firstDay = new Date(
+            curDate.getFullYear(),
+            curDate.getMonth(),
+            1
+        ).getTime();
+
+        const lastDay = new Date(
+            curDate.getFullYear(),
+            curDate.getMonth() + 1,
+            0
+        ).getTime();
+
+        setData(diaryList)
+    }, [diaryList, curDate]);
+
+    useEffect(()=> {
+        console.log(data);
+    }, [data]);
+
+    const increaseMonth = ()=> {
+        setCurDate(
+            new Date(curDate.getFullYear(), curDate.getMonth() + 1, curDate.getDate())
+        );
+    };
+
+    const decreaseMonth = ()=> {
+        setCurDate(
+            new Date(curDate.getFullYear(), curDate.getMonth() - 1, curDate.getDate())
+        );
+    };
+
     return (
         <div>
-            <h2>Home</h2>
-            <p>Home 입니다.</p>
+            <MyHeadr headText={headText}
+            leftChild={<MyButton text={'<'} onClick={decreaseMonth} />}
+            rightChild={<MyButton text={'>'} onClick={increaseMonth} />}
+            />
         </div>
     )
 }
