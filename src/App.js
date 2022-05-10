162f9e365b2095602e47626bb9a4a29554ae4d82
Tavi <secret9179@gmail.com>
2022-05-10 21:15:27 +0900

    페이지 구현 일기쓰기

--- src/App.js
+++ src/App.js
@@ -35,38 +35,7 @@ const reducer = (state, action) => {
 export const DiaryStateContext = React.createContext();
 export const DiaryDispatchContext = React.createContext();
 
-const dummyData = [
-  {
-    id: 1,
-    emotion: 1,
-    content: "오늘의 일기 1번",
-    date: 1652124162812,
-  },
-  {
-    id: 2,
-    emotion: 2,
-    content: "오늘의 일기 2번",
-    date: 1652124162813,
-  },
-  {
-    id: 3,
-    emotion: 3,
-    content: "오늘의 일기 3번",
-    date: 1652124162814,
-  },
-  {
-    id: 4,
-    emotion: 4,
-    content: "오늘의 일기 4번",
-    date: 1652124162815,
-  },
-  {
-    id: 5,
-    emotion: 5,
-    content: "오늘의 일기 5번",
-    date: 1652124162816,
-  },
-];
+const dummyData = [];
 
 function App() {
   const [data, dispatch] = useReducer(reducer, dummyData);
